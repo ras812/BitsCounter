41bb175cd1a97a2efd40e92190ae19922eccda2d
ras812 <69597319+ras812@users.noreply.github.com>
2020-10-12 11:41:52 +0300

    sqiurrel add

--- Squirrel/Squirrel/Properties/AssemblyInfo.cs
+++ Squirrel/Squirrel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleAppCodeWars")]
+[assembly: AssemblyTitle("Squirrel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleAppCodeWars")]
+[assembly: AssemblyProduct("Squirrel")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("981e7e65-3b08-43f8-81a6-3844318146a5")]
+[assembly: Guid("49605e55-9ce5-46e1-bab7-c794bc8fafe1")]
 
 // Version information for an assembly consists of the following four values:
 //
